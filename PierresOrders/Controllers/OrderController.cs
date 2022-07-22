8c8bf5813e5d9c2180714866f6996d0437833352
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-22 16:55:05 -0700

    Add order paths

---
+++ PierresOrders/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PierresOrders.Models;
+
+
+namespace PierresOrders.Controllers
+{
+  public class OrderController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary <string, object> model = new Dictionary <string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+  }
+}
