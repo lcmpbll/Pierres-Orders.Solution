6a69e55560a7c96bdb1c9e639ec85f86e9f57c51
Liam Cammpbell <liam22campbell@gmail.com>
2022-07-22 10:39:08 -0700

    Fail third vendor test

--- PierresOrders.Tests/ModelTests/VendorTests.cs
+++ PierresOrders.Tests/ModelTests/VendorTests.cs
@@ -16,18 +16,28 @@ namespace PierresOrders.Tests
     [TestMethod]//1
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("TestVendor");
+      Vendor newVendor = new Vendor("TestVendor", "Vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
     [TestMethod]//2
     public void GetName_ReturnsName_String()
     {
       string name = "First Vendor";
-      Vendor newVendor = new Vendor(name);
+      Vendor newVendor = new Vendor(name, "vendor");
 
       string result = newVendor.Name;
 
       Assert.AreEqual(result, name);
+    }
+    [TestMethod]//3
+    public void GeDescription_ReturnsDescription_String()
+    {
+      string description = "First Vendor";
+      Vendor newVendor = new Vendor("name", description );
+
+      string result = newVendor.Description;
+
+      Assert.AreEqual(result, description);
     }
 
 
--- PierresOrders/Models/Vendor.cs
+++ PierresOrders/Models/Vendor.cs
@@ -5,10 +5,12 @@ namespace PierresOrders.Models
   public class Vendor
   {
     public string Name { get; set; }
+    public string Description { get; set; }
 
-    public Vendor(string _name)
+    public Vendor(string _name, string _description)
     {
       Name = _name;
+      Description = "Baked goods seller";
     }
 
   }
