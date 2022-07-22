fe17dc39062b03b817ef7c17facce0b8c2b8f95f
lcmpbll <105952041+lcmpbll@users.noreply.github.com>
2022-07-22 08:12:50 -0700

    Initial commit

---
+++ ProjectName/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjectName.Models;
+
+namespace ProjectName.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/")]
+    public ActionResult Index()
+    {
+     
+      return View();
+    }
+
+  }
+}
